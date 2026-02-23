db0caaa0d1c9c02676d6f784b66653cc3e1e033f
sauravkumar0011 <studentlife9341@gmail.com>
2026-02-23 13:00:00 +0530

    [saurav] : Added length file

--- src/main/java/com/quantitymeasurement/Length.java
+++ src/main/java/com/quantitymeasurement/Length.java
@@ -4,12 +4,14 @@ public class Length {
 
 	private final double value;
 	private final LengthUnit unit;
-
+    private static final double EPSILON = 1e-3;
 	public enum LengthUnit {
 
 		FEET(12.0),
-		INCHES(1.0); 
-
+		INCHES(1.0),
+        YARDS(36.0),
+        CENTIMETERS(0.393701);
+        
 		private final double conversionFactor;
 
 		LengthUnit(double conversionFactor) {
@@ -54,8 +56,17 @@ public class Length {
 
 
 	public static void main(String[] args) {
+		
 		Length length1 = new Length(1.0, LengthUnit.FEET);
 		Length length2 = new Length(12.0, LengthUnit.INCHES);
-		System.out.println("Are lengths equal? " + length1.equals(length2));
+		System.out.println("Are lengths equal? " + length1.equals(length2)); // true
+
+		Length length3 = new Length(1.0, LengthUnit.YARDS);
+		Length length4 = new Length(36.0, LengthUnit.INCHES);
+		System.out.println("Are lengths equal? " + length3.equals(length4)); // true
+
+		Length length5 = new Length(100.0, LengthUnit.CENTIMETERS);
+		Length length6 = new Length(39.3701, LengthUnit.INCHES);
+		System.out.println("Are lengths equal? " + length5.equals(length6)); // true
 	}
 }
