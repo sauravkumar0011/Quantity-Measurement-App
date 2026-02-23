28685ee0af98a32b66187559e0a148952079a736
sauravkumar0011 <studentlife9341@gmail.com>
2026-02-23 14:45:37 +0530

    [saurav] : Added length file

--- src/main/java/com/quantitymeasurement/Length.java
+++ src/main/java/com/quantitymeasurement/Length.java
@@ -5,6 +5,7 @@ public class Length {
 	private final double value;
 	private final LengthUnit unit;
     private static final double EPSILON = 1e-3;
+    
 	public enum LengthUnit {
 
 		FEET(12.0),
@@ -40,6 +41,12 @@ public class Length {
 		return value * unit.getConversionFactor();
 	}
 
+	private boolean compare(Length that) {
+		double thisValue = this.convertToBaseUnit();
+		double thatValue = that.convertToBaseUnit();
+		return Math.abs(thisValue - thatValue) < EPSILON;
+	}
+	
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) {
@@ -51,7 +58,36 @@ public class Length {
 		}
 
 		Length that = (Length) o;
-		return Double.compare(this.convertToBaseUnit(), that.convertToBaseUnit()) == 0;
+		return compare(that);
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%.2f %s", value, unit);
+	}
+
+	public Length convertTo(LengthUnit targetUnit) {
+		if (targetUnit == null) {
+			throw new IllegalArgumentException("Target unit cannot be null");
+		}
+		double baseValue = this.convertToBaseUnit();
+		double convertedValue = baseValue / targetUnit.getConversionFactor();
+
+		convertedValue = Math.round(convertedValue * 100.0) / 100.0;
+		return new Length(convertedValue, targetUnit);
+	}
+	
+	public static double convert(double value, LengthUnit source, LengthUnit target) {
+	    if (source == null || target == null) {
+	        throw new IllegalArgumentException("Units cannot be null");
+	    }
+	    if (!Double.isFinite(value)) {
+	        throw new IllegalArgumentException("Value must be a finite number");
+	    }
+
+	    double baseValue = value * source.getConversionFactor();
+	    double convertedValue = baseValue / target.getConversionFactor();
+	    return Math.round(convertedValue * 100.0) / 100.0;
 	}
 
 
@@ -68,5 +104,12 @@ public class Length {
 		Length length5 = new Length(100.0, LengthUnit.CENTIMETERS);
 		Length length6 = new Length(39.3701, LengthUnit.INCHES);
 		System.out.println("Are lengths equal? " + length5.equals(length6)); // true
+		
+		System.out.println("Convert 3 Feet to Inches: " + length1.convertTo(LengthUnit.INCHES));
+		System.out.println("Convert 2 Yards to Inches: " + length3.convertTo(LengthUnit.INCHES));
+		System.out.println("Convert 30.48 cm to Feet: " + new Length(30.48, LengthUnit.CENTIMETERS).convertTo(LengthUnit.FEET));
+		System.out.println("Convert 72 Inches to Yards: " + new Length(72.0, LengthUnit.INCHES).convertTo(LengthUnit.YARDS));
+		System.out.println("Convert 0 Feet to Inches: " + new Length(0.0, LengthUnit.FEET).convertTo(LengthUnit.INCHES));
+		System.out.println("Convert -1 Foot to Inches: " + new Length(-1.0, LengthUnit.FEET).convertTo(LengthUnit.INCHES));
 	}
 }
